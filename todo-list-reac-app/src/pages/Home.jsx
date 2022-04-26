4d6f81a5a816b653621fd6859878b3021b36e5cf
Bruna Luchini <brunaluchini22@gmail.com>
2022-04-25 21:58:55 -0300

    adicionando exercicios de fixação

--- todo-list-reac-app/src/pages/Home.jsx
+++ todo-list-reac-app/src/pages/Home.jsx
@@ -1,24 +1,57 @@
-import { TodoListItem } from '../components/TodoListItem'
-import './Home.css'
+import { useEffect, useState } from 'react';
+import TodoListItem from '../components/TodoListItem';
+import './Home.css';
 
 export function Home() {
-    const itemsArray = [
-        { id: 1, text: "Acordar 7 horas da manhã", checked: false},
-        { id: 2, text: "Lavar toda a louça suja que está dentro ou fora da pia", checked: false},
-        { id: 3, text: "Levar os cachorros para passear por 30 min", checked: false},
-        { id: 4, text: "Lavar o carro todo", checked: true},
+  const [itemsArray, setItemsArray] = useState(() => {
+    const inicialValue = [
+      { text: "Acordar 7 horas da manhã", checked: false },
+      { text: "Lavar toda a louça suja que está dentro ou fora da pia", checked: false },
+      { text: "Levar os cachorros para passear por 30 min", checked: false },
+      { text: "Lavar o carro todo", checked: true },
     ];
 
-    // localStorage.setItem("abc", "oi, tudo bem?");
-    // sessionStorage.setItem("def", "não, ta tudo uma bela bosta!");
-    console.log(itemsArray);
+    const saved = JSON.parse(localStorage.getItem('itemsArray'));
 
-    return (
-        <div className="container">
-            {itemsArray.map(({id, text, checked}) => {
-                {/* return <TodoListItem {...obj} /> */}
-                return <TodoListItem id={id} text={text} key={id} checked={checked}/>
-            })}
+    if (saved === null) return inicialValue;
+    return saved.length >= inicialValue.length ? saved : inicialValue;
+  });
+  const [task, setTask] = useState('');
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    localStorage.setItem('itemsArray', JSON.stringify(itemsArray));
+    setDisabled(task.length < 5);
+  }, [itemsArray, task]);
+
+  return (
+    <>
+      <div className="input-home">
+        <label htmlFor='input-task'>
+          Adicionar tarefa:
+          <input type="text" id="input-task" onChange={({ target }) => setTask(target.value)} />
+        </label>
+        <button
+          onClick={ () => setItemsArray([...itemsArray, { text: task, checked: false }]) }
+          disabled={ disabled }
+        >
+          Enviar
+        </button>
+      </div>
+      <div className="container">
+        <div>
+          {itemsArray.map(({ text, checked }, id) => {
+            return (
+              <TodoListItem
+                id={id}
+                text={text}
+                key={id}
+                checked={checked}
+              />
+            )
+          })}
         </div>
-    )
+      </div>
+    </>
+  )
 }
